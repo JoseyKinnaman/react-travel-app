eaa2e86b67f0c9c622564c0e1d1e23764b202ac1
Jozy Kinnaman <jozypants@gmail.com>
2020-07-21 14:47:15 -0700

    move api call to DEstinationList.

--- src/components/DestinationList.js
+++ src/components/DestinationList.js
@@ -1,38 +1,38 @@
 import React from "react";
-import Destination from "./Destination";
-import PropTypes from "prop-types";
+// import Destination from "./Destination";
+// import PropTypes from "prop-types";
+// import { connect } from 'react-redux';
+// import * as a from './../actions'
+// import { makeApiCall } from '../actions'
 
 function DestinationList(props) {
-  const DestinationListStyles = {
-    backgroundColor: '#F5F5F5',
-    border: 'solid 1pt grey',
-    textAlign: "center",
-    borderRadius: "8px",
-    marginBottom: "20px"
+  const { error, isLoading, destinations } = props.destinations;
+  
+  if (error) {
+    return <React.Fragment>Error</React.Fragment>
+  } else if (isLoading) {
+    return <React.Fragment>Is Loding...</React.Fragment>
+  } else {
+    return (
+      <React.Fragment>
+        <h3 className="card-header">Destinations:</h3>
+        <ul>
+          {destinations.map((destination, index) =>
+            <div class="card text-white bg-dark mb-2">
+              <li key={index}>
+                <h1 className="card-header">{destination.place}</h1>
+                <div className="card-body">
+                  <h5 class="card-title"><em>written by: </em>{destination.author}</h5>
+                  <p className="card-text"><em>Review: </em>{destination.content}</p>
+                </div>
+              </li>
+            </div>
+          )}
+        </ul>
+      </React.Fragment>
+    );
   }
-  return (
-    <React.Fragment>
-      <div style={DestinationListStyles}>
-        <h2>Current Tap List:</h2>
-        {Object.values(props.destinationList).map((destination) => <Destination
-          whenDestinationClicked={props.onDestinationSelection}
-          whenDestinationPurchased={props.onClickingBuy}
-          place={destination.place}
-          author={destination.author}
-          content={destination.content}
-          id={destination.id}
-          key={destination.id}
-        />
-        )}
-      </div>
-    </React.Fragment>
-  );
 }
 
-DestinationList.propTypes = {
-  destinationList: PropTypes.object,
-  onDestinationSelection: PropTypes.func,
-  onClickingBuy: PropTypes.func
-};
 
 export default DestinationList;
--- src/components/TravelControl.js
+++ src/components/TravelControl.js
@@ -6,6 +6,7 @@ import DestinationDetail from './Destination'
 import PropTypes from "prop-types";
 import * as a from './../actions'
 import ReviewForm from './ReviewForm';
+import Destination from './Destination';
 
 class TravelControl extends React.Component {
   constructor(props) {
@@ -54,35 +55,24 @@ class TravelControl extends React.Component {
       currentlyVisibleState = (<ReviewForm onReviewCreation={this.handleAddingReviewToList}/>);
       buttonText = "Return to Destination List"
     } else {
-      currentlyVisibleState = (<DestinationList />)
-    const { error, isLoading, destinations } = this.props;
-    if (error) {
-      return <React.Fragment>Error</React.Fragment>
-    } else if (isLoading) {
-      return <React.Fragment>Is Loding...</React.Fragment>
-    } else {
-      return (
-        <React.Fragment>
-            <h3 className="card-header">Destinations:</h3>
-            <ul>
-              {destinations.map((destination, index) =>
-                <div class="card text-white bg-dark mb-2">
-                  <li key={index}>
-                    <h1 className="card-header">{destination.place}</h1>
-                    <div className="card-body">
-                      <h5 class="card-title"><em>written by: </em>{destination.author}</h5>
-                      <p className="card-text"><em>Review: </em>{destination.content}</p>
-                    </div>
-                  </li>
-                </div>
-              )}
-            </ul>
-        </React.Fragment>
-      );
+      currentlyVisibleState = (<DestinationList destinations={this.props}  />);
+      console.log(this.props.com)
     }
-    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button className="btn btn-dark" onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
   }
 }
+
+TravelControl.propTypes = {
+  destinations: PropTypes.object,
+  isLoading: PropTypes.string,
+  error: PropTypes.string,
+};
+
 const mapStateToProps = state => {
   return {
     destinations: state.destinations,
@@ -91,4 +81,6 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(TravelControl);
+ TravelControl = connect(mapStateToProps)(TravelControl);
+
+export default TravelControl;
